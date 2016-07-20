11fd3383dc9b43aaed59da0d7935516005c11c9a
Arror <763911422@qq.com>
2016-07-20 20:55:24 +0800

    调整输入参数

--- printer/printer.go
+++ printer/printer.go
@@ -1,9 +1,27 @@
 package printer
 
 import (
+	"fmt"
 	"os"
 	"text/template"
+
+	"CodeFly/global"
+	"CodeFly/reader"
 )
+
+// Print 输出文件
+func Print(r *reader.ThriftReader, gci *global.GenerateCommandInfo) error {
+
+	switch gci.Lang {
+	case global.Swift:
+		str := reader.SwiftReader
+		str.InitSwiftThrift(r)
+		printSwiftCodeWith(str)
+		return nil
+	default:
+		return fmt.Errorf("不支持的语言")
+	}
+}
 
 func initTemplate(name string, tmpl string) *template.Template {
 
--- printer/printer_swift.go
+++ printer/printer_swift.go
@@ -6,15 +6,7 @@ import (
 	"CodeFly/reader"
 )
 
-// Print 输出文件
-func Print(r *reader.ThriftReader) error {
-
-	str := reader.SwiftReader
-	str.InitSwiftThrift(r)
-
-	return fmt.Errorf("Error")
-}
-
-func printWith(str *reader.SwiftThriftReader) error {
-	return fmt.Errorf("Error")
+func printSwiftCodeWith(str *reader.SwiftThriftReader) error {
+	fmt.Println("Hallo")
+	return nil
 }
