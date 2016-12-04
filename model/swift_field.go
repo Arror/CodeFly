5d29c4cf828b66cc193e00f908f12dda0a78a168
马强 <maqiang_ios@163.com>
2016-12-04 10:29:32 +0800

    Delete old struct

--- model/swift_field.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// SwiftField Swift Field类型定义
-type SwiftField struct {
-	Type  *SwiftType
-	Name  string
-	Value string
-}
--- model/swift_thrift.go
+++
@@ -1,7 +0,0 @@
-package model
-
-// SwiftThrift Swift Thrift类型定义
-type SwiftThrift struct {
-	Structs  map[string]*SwiftStruct
-	Services map[string]*SwiftService
-}
