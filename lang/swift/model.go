18685240acfa630b661dfe1f79fa41982bb3c373
马强 <maqiang_ios@163.com>
2016-12-13 21:47:31 +0800

    Add Namespaces

--- lang/swift/model.go
+++ lang/swift/model.go
@@ -2,22 +2,26 @@ package swift
 
 import "github.com/arrors/go-thrift/parser"
 
+// Namespaces 名称空间信息
+type Namespaces struct {
+	Namespace        string
+	NamespaceMapping map[string]string
+}
+
 // Enum Swift枚举类型
 type Enum struct {
 	*parser.Enum
-	Namespace string
+	Namespaces
 }
 
 // Struct Swift结构类型
 type Struct struct {
 	*parser.Struct
-	Namespace        string
-	NamespaceMapping map[string]string
+	Namespaces
 }
 
 // Service Swift服务类型
 type Service struct {
 	*parser.Service
-	Namespace        string
-	NamespaceMapping map[string]string
+	Namespaces
 }
