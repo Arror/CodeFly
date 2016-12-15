bc8f68577830355ee490d969d36825c556c6162d
马强 <maqiang_ios@163.com>
2016-12-15 23:19:34 +0800

    Add thrift types

--- global/global.go
+++ global/global.go
@@ -1,4 +1,39 @@
 package global
 
-// Swift Swift language
-const Swift = "swift"
+const (
+	// Swift Swift language
+	Swift = "swift"
+)
+
+const (
+	// ThriftI16 i16
+	ThriftI16 = "i16"
+	// ThriftI32 i32
+	ThriftI32 = "i32"
+	// ThriftI64 i64
+	ThriftI64 = "i64"
+	// ThriftString string
+	ThriftString = "string"
+	// ThriftBool bool
+	ThriftBool = "bool"
+	// ThriftDouble double
+	ThriftDouble = "double"
+	// ThriftByte byte
+	ThriftByte = "byte"
+	// ThriftBinary binary
+	ThriftBinary = "binary"
+)
+
+const (
+	// Set set
+	Set = "set"
+	// Map map
+	Map = "map"
+	// List list
+	List = "list"
+)
+
+const (
+	// Unsupported unsupported
+	Unsupported = "unsupported"
+)
